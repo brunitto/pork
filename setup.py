3923840949be808f6d70ba48f2c896097e2aa730
Bruno Moyle <brunitto@gmail.com>
2017-08-20 20:45:20 -0300

    Essential packaging setup available

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+config = {
+    'name': 'Pork',
+    'description': 'Pork is a Interactive Fiction game engine.'
+    'version': '1.0.0-0',
+    'url': 'https://github.com/brunitto/pork',
+    'download_url': 'https://github.com/brunitto/pork/releases/stable',
+    'author': 'Bruno Moyle',
+    'author_email': 'brunitto@gmail.com',
+    'packages': ['pork'],
+    'install_requires': ['nose', 'web.py'],
+    'scripts': []
+}
